217ce8ef0e63ea3036389984a9d89f55f350f432
hamnahyun <hamnahyun@outlook.com>
2023-03-09 00:56:40 +0900

    polymorphism

---
+++ 23_Polymorphism/objectPointer.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Person
+{
+public:
+    void Sleep() { std::cout << "Sleep" << std::endl; }
+};
+
+class Student : public Person
+{
+public:
+    void Study() { std::cout << "Study" << std::endl; }
+};
+
+class PartTimeStudent : public Student
+{
+public:
+    void work() { std::cout << "Work" << std::endl; }
+};
+
+int main()
+{
+    Person * ptr1 = new Student();
+    Person * ptr2 = new PartTimeStudent();
+    Student * ptr3 = new PartTimeStudent();
+    ptr1 -> Sleep();
+    ptr2 -> Sleep();
+    ptr3 -> Study();
+    delete ptr1;
+    delete ptr2;
+    delete ptr3;
+    return 0;
+}
